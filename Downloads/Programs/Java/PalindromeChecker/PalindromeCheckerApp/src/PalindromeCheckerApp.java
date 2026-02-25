af6c351b33547f3913dc91ee146affee4572befc
SyedGMohammed75 <psychc75@gmail.com>
2026-02-25 10:54:06 +0530

    UC3 completed

--- Downloads/Programs/Java/PalindromeChecker/PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ Downloads/Programs/Java/PalindromeChecker/PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,11 +1,30 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
 
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker App");
-        System.out.println("Version 1.0");
-        System.out.println("Welcome to the application!");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Palindrome Checker App - UC3");
+        System.out.println("Enter a string:");
+
+        String original = scanner.nextLine();
+        String reversed = "";
+
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+
+        if (original.equals(reversed)) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
+
